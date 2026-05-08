68a827ad7d59636a72b7382d96c8f2fb7b41d92c
Andrew Kevin <andrew.kevin.alexander@gmail.com>
2026-05-08 10:47:21 +0700

    Pemisahan Kode agar tidak menjadi God Class

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ProyekIR
 
+Proyek Information Retrieval Implementasi Mesin Pencari Sederhana ini dikerjakan oleh:
+    - Kenneth Nathanael - 6182301018
+    - Gregorius Jason Maresi - 6182301055
+    - Andrew Kevin Alexander - 6182301046
+
+Cara menjalankan program ada 2 cara:
+    1. Menggunakan Tombol Play
+        (a) Buka folder Source_Code
+        (b) Klik file Search.java
+        (c) Setelah file Search.java kebuka, klik tombol play (Run Java di bagian kanan atas)
+        (d) Ketik query yang ingin anda cari di terminal 
+    2. Menggunakan Terminal 
+        (a) Buka terminal 
+        (b) Pastikan terminal berada di path yg benar dengan direktori 
+        (c) Di path blablabla\ProyekIR>, ketik javac Source_Code/*.java
+        (d) Ketik java Source_Code.Search
+        (e) Ketik query yang ingin anda cari di terminal
---
+++ Source_Code/Search.java
@@ -0,0 +1,165 @@
+package Source_Code;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Search {
+    // Stop Word
+    public static Set<String> stopwords = new HashSet<>(Arrays.asList(
+            "a", "an", "and", "are", "as", "at", "be", "by", "for", "from",
+            "has", "he", "in", "is", "it", "its", "of", "on", "that", "the", "to", "was", "were", "will", "with"));
+
+    //public static Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+    public static void main(String[] args) {
+        String path = "./Dokumen"; // Ganti dengan path folder dokumen yang sesuai
+        // I : Data Indexing
+        // 1. Mendapatkan semua file yang ada di folder dokumen
+        File[] files = getAllFiles(path);
+        // 2. Membuat inverted index dari semua file yang ada di folder dokumen
+        HashMap<Integer, String> fileIndex = null;
+        HashMap<String, LinkedList<Integer>> invertedIndex = null;
+        try {
+            // fileIndex u/ menyimpan nama file sebagai nomor
+            fileIndex = new HashMap<>();
+            // Inverted Index
+            invertedIndex = createInvertedIndex(files, fileIndex);
+            // file index tidak di return karena mengirim alamat file ke fungsi
+            // createInvertedIndex, sehingga tidak perlu dikembalikan lagi
+
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+        System.out.println("File Index : " + fileIndex);
+        System.out.println("Inverted Index : " + invertedIndex);
+
+        // Scanner untuk query yang ingin dicari 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nMasukkan kata yang ingin dicari: ");
+        String query = sc.nextLine();
+        sc.close();
+
+        // Query dimasukkan ke dalam array (setiap kata yang dipisah oleh spasi akan dimasukkan ke array)
+        String[] daftarKata = query.split("\\s+");
+        String hasilPreProcessing = "";
+
+        // Looping untuk setiap kata yang ada di query (di array daftarKata)
+        for(String kata: daftarKata){
+            // Setiap kata yang ada di query akan dilakukan preProcessing dan porterStemmer dahulu
+            kata = preProcessing(kata);
+            kata = Stemmer.doPorterStemmer(kata);
+
+            // Jika di inverted index terdapat kata pada query, maka hasil pre processing adalah kata tersebut
+            if(invertedIndex.containsKey(kata)){
+                hasilPreProcessing = kata;
+            } else{ // Jika di inverted index tidak ada kata pada query, maka akan dihitung edit distance antara kata pada query dengan 
+                    // setiap kata di inverted index, lalu hasil pre processing adalah kata yang memiliki edit distance paling kecil dengan kata pada query
+                int minDistance = Integer.MAX_VALUE;
+
+                // Looping ke semua kata (keys) di inverted index
+                for (String kataDiIndex : invertedIndex.keySet()) {
+                    
+                    // Panggil fungsi edit distance untuk menghitung jarak edit distance antara kata pada query dengan kata di index
+                    int jarak = LevenshteinDistance.hitungEditDistance(kata, kataDiIndex);
+                    // Update jika nemu jarak yang lebih kecil
+                    if (jarak < minDistance) {
+                        minDistance = jarak;
+                        hasilPreProcessing = kataDiIndex;
+                    }
+                }
+            }
+            
+            // Cek jika hasil preProcessing tidak kosong
+            if (!hasilPreProcessing.equals("")) {
+                // Jika hasil preProcessing sama dengan kata pada query, maka kata tersebut ditemukan pada indeks
+                if(hasilPreProcessing.equals(kata)){
+                    // Ini ntar bisa dihapus aja, ini cuma buat ngecek hasil preProcessing nya aja 
+                    System.out.println("Query: '" + kata + "' ditemukan.");
+                    System.out.println("Dokumen: " + invertedIndex.get(hasilPreProcessing));
+                } else { //jika hasil preProcessing tidak sama dengan kata pada query, maka kata tersebut tidak ada pada indeks
+                         // dan hasil preProcessing adalah perhitungan dan kata rekomendasi dari edit distance
+
+                    // Ini ntar bisa dihapus aja, ini cuma buat ngecek hasil + biar gampang nanti cek boolean model nya
+                    System.out.println("Query: '" + kata + "' tidak ditemukan.");
+                    System.out.println("Did you mean '" + hasilPreProcessing + "'?");
+                    System.out.println("Dokumen: " + invertedIndex.get(hasilPreProcessing));
+                }
+            } else { // Jika hasil preProcessing kosong, maka kata pada query tidak ditemukan dan tidak ada rekomendasi dari edit distance
+                System.out.printf("Kata '%s' tidak ditemukan di indeks.\n", kata);
+            }
+        }
+    }
+
+    public static File[] getAllFiles(String path) {
+        // Mendapatkan semua file yang ada di folder dokumen
+        File folder = new File(path);
+        // Mendapatkan semua file yang ada di folder dokumen
+        File[] listFiles = folder.listFiles();
+        return listFiles;
+    }
+
+    public static String preProcessing(String word) {
+        // Pre processing sederhana
+        // Mengubah semua menjadi lowercase
+        word = word.toLowerCase();
+        // Menghapus spasi tambahan
+        word = word.trim();
+        // Menghapus tanda baca
+        word = word.replaceAll("[^a-zA-Z]", "");
+        return word;
+    }
+
+    public static HashMap<String, LinkedList<Integer>> createInvertedIndex(File[] files,
+            HashMap<Integer, String> fileIndex) throws FileNotFoundException {
+        HashMap<String, LinkedList<Integer>> invertedIndex = new HashMap<>();
+        Scanner sc;
+        int counter = 0;
+        // Looping semua file yang ada di folder dokumen
+        for (File file : files) {
+            // Bila merupakan sebuah file bertipe teks
+            if (file.isFile() && file.getName().endsWith(".txt")) {
+                // Buat scanner untuk membaca file
+                sc = new Scanner(file);
+                counter++; // Menambahkan nomor indeks untuk setiap file
+                fileIndex.put(counter, file.getName()); // Menyimpan nama file dengan nomor indeks
+                // Looping semua kata yang ada di file
+                while (sc.hasNext()) {
+                    String kata = sc.next(); // Memanggil fungsi preProcessing untuk memproses kata
+                    if (stopwords.contains(kata)) { // Cek apakah kata termasuk stop word atau tidak
+                        continue; // Jika termasuk stop word, lewati kata tersebut
+                    }
+                    // Pre processing
+                    kata = preProcessing(kata);
+                    // Masukkan kata ke porter stemmer untuk mendapatkan bentuk dasar kata
+                    kata = Stemmer.doPorterStemmer(kata);
+
+                    // Memakai LinkedList untuk menyimpan nama file yang mengandung kata tersebut
+                    LinkedList<Integer> tempList;
+                    // Bila belum terdapat di inverted index, maka perlu dimasukkan beserta nama
+                    // filenya
+                    if (!invertedIndex.containsKey(kata)) {
+                        tempList = new LinkedList<>();
+                        // Masukkan nama file ke dalam list
+                        tempList.add(counter);
+                        invertedIndex.put(kata, tempList);
+                    } else {
+                        if (!invertedIndex.get(kata).contains(counter)) {// Cek apakah nama file sudah ada di
+                                                                         // list atau belum
+                            // Masukkan nama file ke dalam list
+                            invertedIndex.get(kata).add(counter);
+                        }
+                    }
+                }
+            }
+        }
+
+        return invertedIndex;
+    }
+
+}
